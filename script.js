dcb9b04303da928a09a1080c2ce68c1966ef8a87
Laura Marques <161334577+Marquesaura@users.noreply.github.com>
2025-06-13 09:51:55 -0300

    manifest e style

--- script.js
+++ script.js
@@ -3,21 +3,37 @@ document.addEventListener('DOMContentLoaded', () => {
   const nextBtn = document.getElementById('next');
   const container = document.getElementById('carousel-container');
 
-  const scrollPerClick = 400; // ajuste conforme o tamanho da imagem
+  let scrollAmount = 0;
+  const scrollPerClick = 220;
 
   prevBtn.addEventListener('click', () => {
-    container.scrollBy({
+    scrollAmount -= scrollPerClick;
+    if (scrollAmount < 0) scrollAmount = 0;
+    container.scrollTo({
       top: 0,
-      left: -scrollPerClick,
+      left: scrollAmount,
       behavior: 'smooth'
     });
   });
 
   nextBtn.addEventListener('click', () => {
+    scrollAmount += scrollPerClick;
+    if (scrollAmount > container.scrollWidth) {
+      scrollAmount = container.scrollWidth;
+    }
     container.scrollBy({
       top: 0,
-      left: scrollPerClick,
+      left: scrollAmount,
       behavior: 'smooth'
     });
   });
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('service-worker.js')
+      .then(() => console.log('Service Worker registrado com sucesso!'))
+      .catch(err => console.error('Erro ao registrar o Service Worker', err));
+  });
+}
